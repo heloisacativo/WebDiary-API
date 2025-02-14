cf497120347966b8b023c5d160a6681815e5d58a
Heloisa Cativo <heloisacativo@gmail.com>
2025-02-14 18:29:36 -0300

    applicationdbcontext config

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebDiary_API.Models;
+
+namespace WebDiary_API.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DiaryEntry_cs> DiaryEntry_Cs { get; set; }
+    }
+}
