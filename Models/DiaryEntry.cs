d2fc10266a7a8c254a5b2a16d00b970cfe2511af
Heloisa Cativo <heloisacativo@gmail.com>
2025-02-14 18:30:21 -0300

    create entity

---
+++ Models/DiaryEntry.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDiary_API.Models
+{
+    public class DiaryEntry_cs
+    {
+        public int Id { get; set; }
+       
+        public string Title { get; set; } = String.Empty;
+        
+        public string Content { get; set; } = String.Empty;
+      
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
